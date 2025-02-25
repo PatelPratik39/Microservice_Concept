bd5993489b3620697a1df8780f13906a3fc86869
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-24 19:25:28 -0600

    Eureka Discovery server set up to both microservices

--- department-service/src/main/java/com/microservices/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/microservices/departmentservice/DepartmentServiceApplication.java
@@ -3,6 +3,7 @@ package com.microservices.departmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
