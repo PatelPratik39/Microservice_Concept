3219931aca2747afeadcda0c07bda8f7749aa224
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-25 19:28:42 -0600

    data file

---
+++ frontend-microservice/src/component/EmployeeComponent.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import EmployeeService from "../service/EmployeeService";
+
+export default class EmployeeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: {},
+      department: {},
+      organization: {}
+    };
+  }
+
+  componentDidMount() {
+    EmployeeService.getEmployee().then((response) => {
+      this.setState({ employee: response.employee });
+      this.setState({ department: response.department });
+      this.setState({ organization: response.organization });
+
+      console.log(this.state.employee);
+      console.log(this.state.department);
+      console.log(this.state.organization);
+    });
+  }
+  render() {
+    return <div></div>;
+  }
+}
