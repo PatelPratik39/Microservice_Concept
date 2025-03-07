d6a599537e3806978626341ecd5bb7f76cc48c6a
PatelPratik39 <ptl.pratik717@gmail.com>
2025-03-07 13:58:00 -0600

    Swagger UI using OpenAPI dependency

--- department-service/src/main/java/com/microservices/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/microservices/departmentservice/DepartmentServiceApplication.java
@@ -1,10 +1,32 @@
 package com.microservices.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version="v1.0",
+				contact = @Contact(
+						name = "Pratik",
+						email = "pratikpatel17791@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc"
+		)
+)
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
